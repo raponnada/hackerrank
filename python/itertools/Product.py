7ffa8c0a23698209c4628aa2a9216208af76f759
Rahul <rponnada91@gmail.com>
2017-11-17 19:04:30 -0600

    Cartesian product of two lists improvised.

--- python/itertools/Product.py
+++ python/itertools/Product.py
@@ -6,6 +6,9 @@ B = map(int, raw_input().split())
 for x, y in list(product(A, B)):
     print (x, y),
 
+# for item in product(A,B):
+#   print item,
+
 # Sol in Python 3
 # for item in product(A, B):
 #    print(item, end=' ')
