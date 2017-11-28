b5e2d4c8b5f1ae06de3637fa96299286f25b3ebb
Rahul <rponnada91@gmail.com>
2017-11-28 15:25:23 -0600

    Mean, Variance and Standard Deviation usnig numpy.

--- python/Numpy/MeanVarSD.py
+++ python/Numpy/MeanVarSD.py
@@ -3,4 +3,6 @@ import numpy
 N, M = map(int, raw_input().strip().split())
 arr_ = numpy.array([map(int, raw_input().strip().split(' ')) for _ in range(N)])
 
-print numpy.max(numpy.min(arr_, axis=1))
+print numpy.mean(arr_, axis=1)
+print numpy.var(arr_, axis=0)
+print numpy.std(arr_, axis=None)
