5bd80ae24ad70b212722ba46f06672a7fc9271c3
Rahul <rponnada91@gmail.com>
2017-12-02 13:41:20 -0600

    Decoding matrix scipt with problem setter solution.

--- python/RegEx/MatrixScript.py
+++ python/RegEx/MatrixScript.py
@@ -23,3 +23,11 @@ for j in range(m):
 
 # print scrpt
 print re.sub(r'(?<=[a-zA-Z0-9])([!@#$%& ])+(?=[a-zA-Z0-9])', ' ', scrpt)
+
+''' Problem setter solution '''
+# import re
+# matrix = []
+# N,M = map(int, raw_input().split())
+# for i in range(N):
+#     matrix.append(list(raw_input()))
+# print re.sub(r'(?<=[A-Za-z0-9])([^A-Za-z0-9]+)(?=[A-Za-z0-9])',' ',"".join("".join(decode) for decode in zip(*matrix)))
